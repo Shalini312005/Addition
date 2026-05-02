2071e6665ee8b422ea0864d5bf78c950fb4885df
Vijayagopal, Deepa <Deepa_Vijayagopal@comcast.com>
2026-05-02 17:18:02 +0530

    altered to 3 number addition

--- src/main/java/org/example/Addition.java
+++ src/main/java/org/example/Addition.java
@@ -5,13 +5,14 @@ public class Addition {
     public static void main(String[] args){
         int a= 10;
         int b= 20;
+        int c=2;
         Addition addObj= new Addition();
-        addObj.sum( a, b);
+        addObj.sum( a, b,c);
     }
 
-     void sum(int a, int b) {
-        int c=a+b;
-        System.out.println("The sum of a and b is: " + c);
+     void sum(int a, int b, int c) {
+        int d=a+b+c;
+        System.out.println("The sum of a and b and c is: " + d);
     }
 
 }
